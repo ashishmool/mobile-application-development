8325bb9ed28b8939efb69a23c0b50df1b59868bc
Ashish Mool <asis.mool@gmail.com>
2024-11-17 10:43:42 +0545

    Map Object
    --simple variable with map operations

---
+++ creatingMap.dart
@@ -0,0 +1,15 @@
+void main() { 
+
+var map1 = new Map();
+
+
+map1 [0] = 'Softwarica';
+map1 [1] = 'College';
+map1 [2] = 'Gyaneshwor';
+
+// Output
+print(map1);
+
+// Printing Specific Index
+print(map[0]); 
+}
